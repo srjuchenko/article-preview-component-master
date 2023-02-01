e4487542ab191fe59cb583def8816a72ab1dde50
srjuchenko <srjuchenko@gmail.com>
2023-02-01 12:30:18 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+const openModalBtn = document.querySelector(".open-modal");
+const closeModalBtn = document.querySelector(".close-modal");
+const modal = document.querySelector(".share-modal");
+
+openModalBtn.addEventListener("click", function () {
+  modal.classList.remove("hidden");
+});
+
+closeModalBtn.addEventListener("click", function () {
+  modal.classList.add("hidden");
+});
